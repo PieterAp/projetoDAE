e8de01cf9029b5c48106c9342fc211dd894448c9
igor-2191815 <2191815@my.ipleiria.pt>
2022-11-30 12:05:55 +0000

    initial commit
    project configuration

---
+++ projectDAE/src/main/java/pt/ipleiria/pt/estg/dei/ei/dae/projectdae/HelloApplication.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.pt.estg.dei.ei.dae.projectdae;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
