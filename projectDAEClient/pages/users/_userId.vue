9231a9f639475f90d0db5d7bed64f24bfd7b569a
unknown <2191820@my.ipleiria.pt>
2022-12-27 00:30:04 +0000

    client project initial commit | teste with users

---
+++ projectDAEClient/pages/users/_userId.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">Academics</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/users">Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container>
+      <h4>User Details:</h4>
+      <p>Name: {{ user.name }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>Address: {{ user.address }}</p>
+      <p>Phone: {{ user.phone }}</p>
+      <nuxt-link to="/users">Back</nuxt-link>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {},
+    }
+  },
+  computed: {
+    userId() {
+      return this.$route.params.userId
+    },
+  },
+  created() {
+    this.$axios.$get(`https://63aa2a6d7d7edb3ae621f6af.mockapi.io/users/${this.userId}`)
+      .then(user => this.user = user || {})
+  },
+}
+</script>
+<style>
+h4 {
+  margin-top: 5%;
+}
+</style>
+
---
+++ projectDAEClient/pages/users/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">Academics</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/users">Users</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container>
+      <div>
+        <b-table striped over :items="users" :fields="fields">
+          <template v-slot:cell(actions)="row">
+            <nuxt-link
+              class="btn btn-link"
+              :to="`/users/${row.item.user_id}`">Details
+            </nuxt-link>
+          </template>
+        </b-table>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      fields: ['user_id','name', 'email', 'actions'],
+      users: []
+    }
+  },
+  created() {
+    this.$axios.$get('https://63aa2a6d7d7edb3ae621f6af.mockapi.io/users/')
+      .then((users) => {
+        this.users = users
+      })
+  }
+}
+</script>
+
